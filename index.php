38a912ce41b42747562f1800eae918f5c161a872
ManikantaSatyaSubbareddy <50864579+subbareddykovvuri@users.noreply.github.com>
2021-07-12 20:45:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-
-$con=mysqli_connect("MARIADB_SERVICE_HOST","root","","student_registration");
+$host=getenv("MARIADB_SERVICE_HOST");
+$user=getenv("db-user");
+$pass=getenv("db-password");
+$dbname=getenv("db-name");
+$con=mysqli_connect($host,$user,$pass,$dbname);
 $row="SELECT * FROM news order by Time DESC";
 $re=mysqli_query($con,$row);
  
