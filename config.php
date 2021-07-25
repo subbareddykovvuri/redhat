6f8b33baeede919213fc0c12bdb66546337671fb
ManikantaSatyaSubbareddy <50864579+subbareddykovvuri@users.noreply.github.com>
2021-07-25 19:45:20 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 $host=getenv("MARIADB_SERVICE_HOST");
 $user=getenv("db-user");
 $pass=getenv("db-password");
-$con=mysqli_connect($host,$user,$pass,"student_registration");
+$con=mysqli_connect($host,"subba","subba","student_registration");
 ?>
